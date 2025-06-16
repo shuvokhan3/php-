ef6d73c6003bfab0de87ffd9abd826a7d87b9c1a
shuvokhan3 <shuvoj360@gmail.com>
2025-06-16 22:29:23 +0600

    Inheritance practice

--- oop/inheritance.php
+++ oop/inheritance.php
@@ -1,36 +1,23 @@
 <?php
-
-class Song{
-    public $name;
-    public $id;
-    public function __construct($name, $id){
-        $this->name = $name;
-        $this->id   = $id;
-    }
-
-    public function SongName(){
-        return $this->name. $this->id;
-    }
-    //here this singerName() method is static so it can not possible to overridding this method on the child class
-    static public function singerName(){
-        return "Atif";
-    }
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
 }
 
-class playList extends song{
-    public $playListName;
-    public function __construct($playListName,$name, $id){
-        parent::__construct($name, $id);//here i assign the value of the parent class property
-        $this->playListName = $playListName;
-    }
-
-    public function playListInfo(){
-        return "Playlist name is : ".$this->playListName. "Song and song id is : ".$this->name.$this->id;
-    }
-
+// Strawberry is inherited from Fruit
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? ";
+  }
 }
-
-$favList = new playList("hard rock","Shuvvooo",101);
-
-echo $favList->singerName();
-
+$strawberry = new Strawberry("Strawberry", "red");
+$strawberry->message();
+$strawberry->intro();
+?>
